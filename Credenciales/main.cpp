d0d47ef0b50196d9eeaacbd909f321f08e014fd5
Xarli11 <50557033+Xarlie11@users.noreply.github.com>
2021-01-16 20:34:15 +0100

    Añadido la comprobacion por array aunque cuando pones algo erroneo no funciona

--- Credenciales/main.cpp
+++ Credenciales/main.cpp
@@ -14,7 +14,6 @@ using namespace std;
 
 
 /*Estructuras y Constantes Globales*/
-const string USUARIO = "Xarli11", CONTRASEÑA = "Carlosgt2001";
 
 typedef struct {
     string usuario;
@@ -27,6 +26,7 @@ void login();
 
 void menu();
 void leer(tListaCredenciales& Credenciales);
+bool comprobar(string usuario, tListaCredenciales &Credendiales, int contador);
 
 int main() {
     login();
@@ -35,10 +35,10 @@ int main() {
 
 void login() {
     string nombre;
-    string pass;
     tListaCredenciales credenciales;
+    leer(credenciales);
     int i;
-    for (i = 1; i < 4; i++) {
+    for (i= 1; i < 4; i++) {
         system("cls");
         cout << endl;
         cout << "        BIENVENIDO AL SISTEMA DE LOGIN " << endl;
@@ -48,43 +48,13 @@ void login() {
         cout << endl;
         cout << "¿Nombre de usuario?" << endl;
         cin >> nombre;
-        if (nombre == USUARIO) {
-            cout << "Nombre de usuario correcto, por favor ingrese su contrasena:" << endl;
-            cin >> pass;
-            if (pass == CONTRASEÑA) {
-                cout << endl;
-                cout << "Contrasena correcta, bienvenido, sera redirigido a la aplicacion" << endl;
-                //menu();
-                leer(credenciales);
-                system("pause>null");
-                exit(0);
-            }
-            else
-                if (pass != CONTRASEÑA) {
-                    cout << "Contrasena incorrecta, por favor ingrese un usuario y contrasena valida" << endl;
-                    system("pause>null");
-                }
+        if (comprobar(nombre, credenciales,i) == true) {
+            cout << "Contrasena correcta, bienvenido, sera redirigido a la aplicacion" << endl;
         }
-        else
-            if (nombre != USUARIO) {
-                cout << "Nombre de usuario incorrecto, por favor ingrese su nombre de usuario nuevamente" << endl;
-                system("pause>null");
-            }
-
-        if (i > 0) {
-            cout << endl;
-            cout << "Ha realizado " << i << "/3 intentos" << endl;
-            cout << endl;
-            system("pause>null");
-        }
-        if (i == 3) {
-            cout << endl;
-            cout << "Ha realizado 3 intentos, por seguridad el programa se cerrara..." << endl;
-            system("pause>null");
-            exit(0);
+        else {
+            cout << "Contrasena incorrecta, sera expulsado de la aplicacion" << endl;
         }
     }
-
 }
 
 void menu() {
@@ -109,3 +79,52 @@ void leer(tListaCredenciales& Credenciales) {
     }
 
 }
+bool comprobar(string usuario, tListaCredenciales& Credenciales, int contador) {
+        string pass;
+        int numUsuario;
+        bool encontradoUsuario = false, encontradoContraseña = false;
+        int j,k;
+        do {
+            for (j = 0; j < 10; j++) {
+                if (Credenciales[j].usuario == usuario) {
+                    encontradoUsuario = true;
+                    numUsuario = j;
+                }
+            }
+            }while (encontradoUsuario == false || j < 10);
+        if (encontradoUsuario == true) {
+            cout << "Nombre de usuario correcto, por favor ingrese su contrasena:" << endl;
+            cin >> pass;
+            if (Credenciales[numUsuario].contraseña==pass) {
+                encontradoContraseña = true;
+            }
+            if (encontradoContraseña == true) {
+                cout << endl;
+                cout << "Contrasena correcta, bienvenido, sera redirigido a la aplicacion" << endl;
+                //menu();
+                system("pause>null");
+                exit(0);
+            }
+            else {
+                cout << "Contrasena incorrecta, por favor ingrese un usuario y contrasena valida" << endl;
+                system("pause>null");
+            }
+        }
+        else {
+            cout << "Nombre de usuario incorrecto, por favor ingrese su nombre de usuario nuevamente" << endl;
+            system("pause>null");
+        }
+
+        if (contador > 0) {
+            cout << endl;
+            cout << "Ha realizado " << contador << "/3 intentos" << endl;
+            cout << endl;
+            system("pause>null");
+        }
+        if (contador == 3) {
+            cout << endl;
+            cout << "Ha realizado 3 intentos, por seguridad el programa se cerrara..." << endl;
+            system("pause>null");
+            exit(0);
+        }
+    }
